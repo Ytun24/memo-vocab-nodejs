627996846d49fe50de925cd5520dd80c0c5a17ed
ginying <>
2024-03-18 23:36:00 +0700

    Install Express and Apply middleware

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
   res.write("<html>");
   res.write("<h1>Welcome!</h1>");
   res.write("</html>");
   res.end();
 });
+
+const server = http.createServer(app);
 
 server.listen(3000);
