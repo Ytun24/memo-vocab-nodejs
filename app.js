9a89a77ecfdb41f518d1801deed0e38700bf7731
ginying <>
2024-03-19 22:19:51 +0700

    Install and Apply body-parser

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require("http");
-
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
@@ -10,6 +10,7 @@ app.use((req, res, next) => {
   res.write("</html>");
   res.end();
 });
+app.use(bodyParser.urlencoded());
 
 const server = http.createServer(app);
 
