e75af5c86885d8f805a61ed9f04d322f720f0561
ginying <>
2024-03-19 22:30:45 +0700

    Add routes and not found handling

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const http = require("http");
 const express = require("express");
 const bodyParser = require("body-parser");
+const memberRoutes = require("./routes/member")
+const showcaseRoutes = require("./routes/showcase")
+
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.write("<html>");
-  res.write("<h1>Welcome!</h1>");
-  res.write("</html>");
-  res.end();
-});
 app.use(bodyParser.urlencoded());
 
+app.use('/member', memberRoutes);
+app.use('/showcase', showcaseRoutes);
+
+app.use('/', (req, res) => {
+  res.status(404).send("not found")
+});
 const server = http.createServer(app);
 
 server.listen(3000);
