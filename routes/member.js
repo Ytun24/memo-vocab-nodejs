81d3ce1722d08e238bb77df2bfa9777cbef7ab65
ginying <>
2024-03-20 21:34:42 +0700

    Add serving HTML response

--- routes/member.js
+++ routes/member.js
@@ -2,7 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
-router.post('/add-vocab', (req, res, next) => {
+const path = require("path");
+
+router.get("/add-vocab", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "..", "views", "add-vocab.html"));
+});
+
+router.post("/add-vocab", (req, res, next) => {
   res.send(req.body);
 });
 
